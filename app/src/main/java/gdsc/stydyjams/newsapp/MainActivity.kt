563f0dbd5856b087f7d458e45001d61c53cd047f
Prasad <thakareprasad28@yahoo.in>
2021-12-05 22:52:27 +0530

    login signup activity added

--- app/src/main/java/gdsc/stydyjams/newsapp/MainActivity.kt
+++ app/src/main/java/gdsc/stydyjams/newsapp/MainActivity.kt
@@ -1,21 +1,23 @@
-package gdsc.stydyjams.newapp
+package gdsc.stydyjams.newsapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var loggedIn = false
-        //dashboard
-        if(!loggedIn)
-        {
-            //redirect to the login page
+        // check if user is logged in
+        val loggedIn = false
+        if (!loggedIn) {
+            // send to login page
             val loginIntent = Intent(this, LoginActivity::class.java)
             startActivity(loginIntent)
+            this.finish()
         }
+        // else stay here
+
     }
 }
