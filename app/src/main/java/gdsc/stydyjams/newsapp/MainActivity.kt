24512d7087ef4a61a9784d582f869ecfe1fc9870
Azeez <officialazeezamd@gmail.com>
2021-12-09 20:13:56 +0530

    added recycler view

--- app/src/main/java/gdsc/stydyjams/newsapp/MainActivity.kt
+++ app/src/main/java/gdsc/stydyjams/newsapp/MainActivity.kt
@@ -3,21 +3,45 @@ package gdsc.stydyjams.newsapp
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import gdsc.stydyjams.newsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        // check if user is logged in
-        val loggedIn = false
-        if (!loggedIn) {
-            // send to login page
-            val loginIntent = Intent(this, LoginActivity::class.java)
-            startActivity(loginIntent)
-            this.finish()
-        }
-        // else stay here
+        var news = mutableListOf(
+            HeadlineItem("India wins a Gold Medal", R.drawable.img1),
+            HeadlineItem("India wins a Gold Medal", R.drawable.img1),
+            HeadlineItem("India wins a Gold Medal", R.drawable.img1),
+            HeadlineItem("India wins a Gold Medal", R.drawable.img1),
+            HeadlineItem("India wins a Gold Medal", R.drawable.img1),
+            HeadlineItem("India wins a Gold Medal", R.drawable.img1),
+            HeadlineItem("India wins a Gold Medal", R.drawable.img1),
+            HeadlineItem("India wins a Gold Medal", R.drawable.img1),
+            HeadlineItem("India wins a Gold Medal", R.drawable.img1),
+            HeadlineItem("India wins a Gold Medal", R.drawable.img1),
+            HeadlineItem("India wins a Gold Medal", R.drawable.img1),
+        )
+
+        val adapter = RecyclerViewAdapter(news)
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+
+//        // check if user is logged in
+//        val loggedIn = false
+//        if (!loggedIn) {
+//            // send to login page
+//            val loginIntent = Intent(this, LoginActivity::class.java)
+//            startActivity(loginIntent)
+//            this.finish()
+//        }
+//        // else stay here
 
     }
 }
