7cefcb6dbb4bf5d2a29e00a907625fa1428152a4
pk-218 <2182001pk@gmail.com>
2022-01-03 15:22:22 +0530

    Added bookmark ImageButton to recycler_view_item.xml and modified related RecyclerView code

--- app/src/main/java/gdsc/stydyjams/newsapp/RecyclerViewAdapter.kt
+++ app/src/main/java/gdsc/stydyjams/newsapp/RecyclerViewAdapter.kt
@@ -1,36 +1,53 @@
 package gdsc.stydyjams.newsapp
 
+import android.content.Context
 import android.graphics.Color
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.autofill.AutofillValue
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import gdsc.stydyjams.newsapp.databinding.RecyclerViewItemBinding
-import androidx.navigation.findNavController
 import gdsc.stydyjams.newsapp.network.Article
 
 
 class RecyclerViewAdapter(
-    var news : List<Article>
-) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    private val context: Context,
+    var news: List<Article>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     private lateinit var binding: RecyclerViewItemBinding
+
     inner class RecyclerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_view_item, parent, false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.recycler_view_item, parent, false)
         binding = RecyclerViewItemBinding.bind(view)
         return RecyclerViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        if(position%2==0) {
-            binding.headlineText.setTextColor(Color.DKGRAY)
-        } else {
-            binding.headlineText.setTextColor(Color.BLACK)
+//        if (position % 2 == 0) {
+//            binding.headlineText.setTextColor(Color.DKGRAY)
+//        } else {
+//            binding.headlineText.setTextColor(Color.BLACK)
+//        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            binding.root.setBackgroundColor(
+                context.resources.getColor(
+                    R.color.primary_background,
+                    context.theme
+                )
+            )
         }
+        binding.headlineText.setTextColor(Color.LTGRAY)
         binding.headlineText.text = news[position].title
-        //binding.headlineImage.setImageResource(news[position].urlToImage)
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            binding.slotBookmark.autofill(AutofillValue.forToggle(false))
+        }
+//        binding.headlineImage.setImageResource(news[position].urlToImage)
 
         holder.itemView.setOnClickListener {
 //                val activity = v!!.context as AppCompatActivity
@@ -39,8 +56,6 @@ class RecyclerViewAdapter(
 //                .addToBackStack(null).commit()
             val action = ListFragmentDirections.actionListFragmentToNewsFragment2()
             holder.itemView.findNavController().navigate(action)
-
-
         }
     }
 
